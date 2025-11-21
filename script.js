67f9b363d5f9387d7425a5fbbe6f36d9464237d9
KSarai97 <47020076+KSarai97@users.noreply.github.com>
2025-11-21 03:56:37 -0600

    cod multijugador parte 1
    codigo

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
 // =================================================================================================
 // DECLARACIÓN DE VARIABLES
 // =================================================================================================
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCWuR1EeefTapQE2qQpoYzaz7R7KwYLnwU",
+    authDomain: "gcw-cyberrun.firebaseapp.com",
+    databaseURL: "https://gcw-cyberrun-default-rtdb.firebaseio.com",
+    projectId: "gcw-cyberrun",
+    storageBucket: "gcw-cyberrun.firebasestorage.app",
+    messagingSenderId: "674852434984",
+    appId: "1:674852434984:web:c920345a5d5c7f0875311d"
+  };
+
+  import { getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider 
+  } from "firebase/auth";
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+const btnLogin = document.getElementBy("btn-login");
+const btnLogout = document.getElementBy("btn-logout");
+
+async function login(){
+  await (auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+}
+
+btnLogin.addEventListener("click", async() =>{
+  await login();
+});
 
 // AUDIO -----------------------
 const musica = document.getElementById("musica-fondo");
